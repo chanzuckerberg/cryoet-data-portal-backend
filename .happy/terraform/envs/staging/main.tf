57526fa00ceed92eaacf7c262d5ea5eb8ee0a892
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2024-02-07 18:06:59 -0800

    Updating tf to match new spec

--- .happy/terraform/envs/staging/main.tf
+++ .happy/terraform/envs/staging/main.tf
@@ -10,11 +10,11 @@ module "stack" {
   source           = "git@github.com:chanzuckerberg/happy//terraform/modules/happy-stack-eks?ref=main"
   image_tag        = var.image_tag
   image_tags       = jsondecode(var.image_tags)
+  app_name         = "cryoet"
   stack_name       = var.stack_name
   deployment_stage = "staging"
   stack_prefix     = "/${var.stack_name}"
   k8s_namespace    = var.k8s_namespace
-  platform_architecture = "arm64"
   additional_env_vars = {
     HASURA_GRAPHQL_ENABLE_CONSOLE = "false"
     HASURA_GRAPHQL_DATABASE_URL = data.aws_ssm_parameter.db_url.value
@@ -29,7 +29,8 @@ module "stack" {
       memory            = "1500Mi"
       cpu               = "1500m"
       health_check_path = "/healthz",
-      service_type      = "EXTERNAL"
+      service_type      = "EXTERNAL",
+      platform_architecture = "arm64",
     }
   }
   tasks = {
