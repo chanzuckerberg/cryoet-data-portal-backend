5fc2111f9144736a062a4cf8919a6856a4898306
uermel <uermel@czii.org>
2024-03-05 17:45:35 -0800

    LinkML schema, documentation and codegen.
    revert local change

--- test_infra/seed_moto.sh
+++ test_infra/seed_moto.sh
@@ -2,7 +2,7 @@
 
 # Script to seed moto server; runs outside the motoserver container for development
 
-aws="aws --endpoint-url=http://localhost:4001"
+aws="aws --endpoint-url=http://localhost:4000"
 export AWS_ACCESS_KEY_ID=test
 export AWS_SECRET_ACCESS_KEY=test
 export AWS_REGION=us-west-2
