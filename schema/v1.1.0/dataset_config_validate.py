e8e613af98ff22a899477eb932b096882f088073
Daniel Ji <daji@ucsd.edu>
2024-07-22 19:21:56 -0700

    add field whitelist feature to dataset config validation

--- schema/v1.1.0/dataset_config_validate.py
+++ schema/v1.1.0/dataset_config_validate.py
@@ -21,6 +21,7 @@ logger.setLevel(logging.INFO)
 
 DATASET_CONFIGS_DIR = "../../ingestion_tools/dataset_configs/"
 ERRORS_OUTPUT_DIR = "./dataset_config_validate_errors"
+FIELD_WHITELIST_FILE = "dataset_config_field_whitelist.json"
 
 # The permitted parent attribute for formatted strings and its corresponding depth
 # If the attribute has a parent attribute in this list, it is allowed to be a formatted string
@@ -110,6 +111,12 @@ def replace_formatted_strings(config_data: dict, depth: int, permitted_parent: b
     help="Exclude files that contain the following keywords in the filename, used in conjunction with --input-dir. Repeat the flag for multiple keywords.",
 )
 @click.option(
+    "--field-whitelist-file",
+    type=str,
+    default=FIELD_WHITELIST_FILE,
+    help="Path to the whitelist file containing class-field-value mappings to ignore during validation. See docs for more information.",
+)
+@click.option(
     "--output-dir",
     type=str,
     default=ERRORS_OUTPUT_DIR,
@@ -126,12 +133,13 @@ def main(
     input_dir: str,
     include_glob: str,
     exclude_keywords: str,
+    field_whitelist_file: str,
     output_dir: str,
     network_validation: bool,
     verbose: bool,
 ):
     """
-    See ingestion_tools/docs/dataset_config_validation.md for more information.
+    See ingestion_tools/docs/dataset_config_validate.md for more information.
     """
     if verbose:
         logger.setLevel(logging.DEBUG)
@@ -156,6 +164,10 @@ def main(
         logger.warning("No files to validate.")
         return
 
+    with open(field_whitelist_file, "r") as f:
+        field_whitelist = json.load(f)
+    logger.debug("Using field whitelist: %s", field_whitelist_file)
+
     # Remove existing dir
     if os.path.exists(output_dir):
         logging.warning("Removing existing %s directory.", output_dir)
@@ -175,7 +187,11 @@ def main(
                 # formatted strings and the base type in the same field)
                 # https://github.com/linkml/linkml/issues/1521
                 config_data = replace_formatted_strings(config_data, 0, False)
-                ExtendedValidationContainer(**config_data, network_validation=network_validation)
+                ExtendedValidationContainer(
+                    **config_data,
+                    network_validation=network_validation,
+                    field_whitelist=field_whitelist,
+                )
         except ValidationError as e:
             validation_succeeded = False
             # Get all errors and convert them to strings
