3377493be7649d9ae5bab7f58c6d4c175d42a20d
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2024-08-06 09:21:12 -0700

    Updating the comments text

--- api_server/migrations/cryoetdataportal/1721456233007_create_table_public_depositions/up.sql
+++ api_server/migrations/cryoetdataportal/1721456233007_create_table_public_depositions/up.sql
@@ -20,8 +20,9 @@ COMMENT ON COLUMN public.depositions.description IS 'Description for the deposit
 COMMENT ON COLUMN public.depositions.release_date IS 'The date the deposition was released';
 COMMENT ON COLUMN public.depositions.last_modified_date IS 'The date the deposition was last modified';
 COMMENT ON COLUMN public.depositions.deposition_date IS 'The date the deposition was deposited';
-COMMENT ON COLUMN public.depositions.deposition_publications IS 'The publications related to this deposition';
 COMMENT ON COLUMN public.depositions.related_database_entries IS 'The related database entries to this deposition';
+COMMENT ON COLUMN public.depositions.deposition_publications IS 'The publications related to this deposition';
+COMMENT ON COLUMN public.depositions.deposition_types IS 'The types of data submitted as a part of this deposition';
 COMMENT ON COLUMN public.depositions.s3_prefix IS 'The S3 public bucket path where data about this deposition is contained';
 COMMENT ON COLUMN public.depositions.https_prefix IS 'The https directory path where data about this deposition is contained';
 
