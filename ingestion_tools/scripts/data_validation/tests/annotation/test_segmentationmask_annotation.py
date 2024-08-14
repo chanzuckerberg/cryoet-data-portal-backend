1456a5ae76a76c6e04229d0d39aaf606f02efde7
Daniel Ji <daji@ucsd.edu>
2024-08-13 19:33:41 -0700

    refactor annotation validation

--- ingestion_tools/scripts/data_validation/tests/annotation/test_segmentationmask_annotation.py
+++ ingestion_tools/scripts/data_validation/tests/annotation/test_segmentationmask_annotation.py
@@ -19,63 +19,74 @@ class TestSegmentationMaskHeader:
     """Validate the mrc file header for a volume annotation."""
 
     ### BEGIN MRC Self-consistency tests ###
-    def header_helper(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter], check_func: callable, **kwargs):
+    def mrc_header_helper(
+        self,
+        seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter],
+        check_func: callable,
+        **kwargs,
+    ):
         """Helper function to check the header of the mrc file."""
         for mrc_filename, interpreter in seg_mask_annotation_mrc_headers.items():
-            print(f"MRC File: {mrc_filename}")
-            check_func(interpreter.header, interpreter, **kwargs)
+            print(f"Checking {mrc_filename}")
+            try:
+                check_func(interpreter.header, interpreter, mrc_filename, **kwargs)
+            except Exception as e:
+                pytest.fail(f"Error in file {mrc_filename}, with kwargs {kwargs}: {e}")
 
     def test_is_volume(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the mrc file is a volume."""
 
-        def check_is_volume(header, _):
+        def check_is_volume(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             assert header.ispg == self.VOLUME_SPACEGROUP
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_is_volume)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_is_volume)
 
     def test_map_id_string(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the MAP ID is correct."""
 
-        def check_map_id(header, _):
+        def check_map_id(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             assert header.map == self.MAP_ID
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_map_id)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_map_id)
 
     def test_machine_stamp(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the machine stamp is valid."""
 
-        def check_machine_stamp(header, _):
+        def check_machine_stamp(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             try:
                 utils.byte_order_from_machine_stamp(header.machst)
-            except ValueError:
-                pytest.fail(f"Machine stamp is invalid: {utils.pretty_machine_stamp(header.machst)}")
+            except ValueError as e:
+                raise AssertionError(f"Machine stamp is invalid: {utils.pretty_machine_stamp(header.machst)}") from e
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_machine_stamp)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_machine_stamp)
 
     def test_mrc_mode(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the mrc mode is valid."""
 
-        def check_mrc_mode(header, _):
-            try:
-                assert utils.dtype_from_mode(header.mode) == np.int8
-            except ValueError:
-                pytest.fail(f"Mode is invalid: {header.mode}")
+        def check_mrc_mode(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
+            assert utils.dtype_from_mode(header.mode) == np.int8
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_mrc_mode)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_mrc_mode)
 
     def test_map_dimension_fields(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the map dimension fields are non-negative."""
 
-        def check_map_dimension_fields(header, _):
+        def check_map_dimension_fields(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             for field in ["nx", "ny", "nz", "mx", "my", "mz", "ispg", "nlabl"]:
                 assert header[field] >= 0
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_map_dimension_fields)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_map_dimension_fields)
 
     def test_cell_dimensions(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the cell dimensions are non-negative and match the voxel size."""
 
-        def check_cell_dimensions(header, interpreter):
+        def check_cell_dimensions(header, interpreter, _mrc_filename):
+            del _mrc_filename
             assert header.cella.x >= 0 and header.cella.x == pytest.approx(
                 header.mx * interpreter.voxel_size["x"].astype(float),
             )
@@ -86,12 +97,13 @@ class TestSegmentationMaskHeader:
                 header.mz * interpreter.voxel_size["z"].astype(float),
             )
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_cell_dimensions)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_cell_dimensions)
 
     def test_nlabel(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the nlabel is correct."""
 
-        def check_nlabel(header, _):
+        def check_nlabel(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             count = 0
             empty_label = False
             for label in header.label:
@@ -104,20 +116,22 @@ class TestSegmentationMaskHeader:
             assert count == header.nlabl
             assert empty_label is False
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_nlabel)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_nlabel)
 
     def test_nversion(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the nversion is correct."""
 
-        def check_nversion(header, _):
+        def check_nversion(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             assert header.nversion in [20140, 20141]
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_nversion)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_nversion)
 
     def test_exttyp(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the exttyp is correct."""
 
-        def check_exttyp(header, interpreter):
+        def check_exttyp(header, interpreter, _mrc_filename):
+            del _mrc_filename
             if header.nsymbt > 0:
                 assert header.exttyp in [b"CCP4", b"MRCO", b"SERI", b"AGAR", b"FEI1", b"FEI2", b"HDF5"]
             assert (
@@ -126,7 +140,7 @@ class TestSegmentationMaskHeader:
                 or header.nsymbt == interpreter.extended_header.nbytes
             )
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_exttyp)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_exttyp)
 
     def test_spacing_consistent(
         self,
@@ -135,7 +149,8 @@ class TestSegmentationMaskHeader:
     ):
         """Check that the voxel spacing is consistent with the mrc header."""
 
-        def check_spacing(_, interpreter, voxel_spacing):
+        def check_spacing(_header, interpreter, _mrc_filename, voxel_spacing):
+            del _header, _mrc_filename
             assert voxel_spacing == pytest.approx(
                 interpreter.voxel_size["x"].astype(float),
                 abs=self.SPACING_TOLERANCE,
@@ -149,23 +164,25 @@ class TestSegmentationMaskHeader:
                 abs=self.SPACING_TOLERANCE,
             )
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_spacing, voxel_spacing=voxel_spacing)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_spacing, voxel_spacing=voxel_spacing)
 
     def test_axis_mapping(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the axis mapping is x == col, y == row, z == sec."""
 
-        def check_axis_mapping(header, _):
+        def check_axis_mapping(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             # Standard axis mapping
             assert header.mapc == 1
             assert header.mapr == 2
             assert header.maps == 3
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_axis_mapping)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_axis_mapping)
 
     def test_unit_cell_valid_for_3d_volume(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the unit cell is valid for a volume."""
 
-        def check_unit_cell(header, _):
+        def check_unit_cell(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             # Unit cell z-size is 1
             assert header.mx == header.nx
             assert header.my == header.ny
@@ -176,43 +193,61 @@ class TestSegmentationMaskHeader:
             assert header.cellb.beta == 90
             assert header.cellb.gamma == 90
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_unit_cell)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_unit_cell)
 
     def test_origin_is_zero(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the origin is zero."""
 
-        def check_origin(header, _):
+        def check_origin(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             # Origin is zero
             assert header.origin.x == 0
             assert header.origin.y == 0
             assert header.origin.z == 0
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_origin)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_origin)
 
     def test_subimage_start_is_zero(self, seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter]):
         """Check that the subimage start is zero for a volume."""
 
-        def check_subimage_start(header, _):
+        def check_subimage_start(header, _interpreter, _mrc_filename):
+            del _interpreter, _mrc_filename
             # Subimage start is zero
             assert header.nxstart == 0
             assert header.nystart == 0
             assert header.nzstart == 0
 
-        self.header_helper(seg_mask_annotation_mrc_headers, check_subimage_start)
+        self.mrc_header_helper(seg_mask_annotation_mrc_headers, check_subimage_start)
 
     ### END MRC Self-consistency tests ###
 
     ### BEGIN ZARR Self-consistency tests ###
+    def zarr_header_helper(
+        self,
+        seg_mask_annotation_zarr_headers: Dict[str, Dict[str, Dict]],
+        check_func: callable,
+        **kwargs,
+    ):
+        """Helper function to check the header of the zarr file."""
+        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
+            print(f"Checking {zarr_filename}")
+            try:
+                check_func(header_data, zarr_filename, **kwargs)
+            except Exception as e:
+                pytest.fail(f"Error in file {zarr_filename}, with kwargs {kwargs}: {e}")
+
     def test_zarr_zattrs_path(
         self,
         seg_mask_annotation_zarr_headers: Dict[str, Dict[str, Dict]],
     ):
         """Check that the path is correct for a zarr annotation file."""
-        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
-            print(f"Zarr File: {zarr_filename}")
 
+        def check_zattrs_path(header_data, _zarr_filename):
+            del _zarr_filename
             for i in range(3):
                 assert header_data["zattrs"]["multiscales"][0]["datasets"][i]["path"] == str(i)
+
+        self.zarr_header_helper(seg_mask_annotation_zarr_headers, check_zattrs_path)
 
     def test_zarr_zattrs_voxel_spacings(
         self,
@@ -220,21 +255,27 @@ class TestSegmentationMaskHeader:
         voxel_spacing,
     ):
         """Check that the voxel spacings are correct for a zarr annotation file."""
-        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
-            print(f"Zarr File: {zarr_filename}")
 
+        def check_zattrs_voxel_spacings(header_data, _zarr_filename, voxel_spacing):
+            del _zarr_filename
             for i in range(3):
                 datasets_entry = header_data["zattrs"]["multiscales"][0]["datasets"][i]
                 assert datasets_entry["coordinateTransformations"][0]["scale"] == [voxel_spacing * (2**i)] * 3
+
+        self.zarr_header_helper(
+            seg_mask_annotation_zarr_headers,
+            check_zattrs_voxel_spacings,
+            voxel_spacing=voxel_spacing,
+        )
 
     def test_zarr_zattrs_axes(
         self,
         seg_mask_annotation_zarr_headers: Dict[str, Dict[str, Dict]],
     ):
         """Check that the voxel spacings are correct for a zarr annotation file."""
-        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
-            print(f"Zarr File: {zarr_filename}")
 
+        def check_zattrs_axes(header_data, _zaar_filename):
+            del _zaar_filename
             assert len(header_data["zattrs"]["multiscales"][0]["axes"]) == 3
             for i, axis in enumerate(self.ZATTRS_AXIS_ORDER):
                 assert header_data["zattrs"]["multiscales"][0]["axes"][i] == {
@@ -243,16 +284,19 @@ class TestSegmentationMaskHeader:
                     "unit": "angstrom",
                 }
 
+        self.zarr_header_helper(seg_mask_annotation_zarr_headers, check_zattrs_axes)
+
     def test_zarr_zarray(self, seg_mask_annotation_zarr_headers: Dict[str, Dict[str, Dict]]):
         """Check that the data type is correct for each binning / scale of the zarr annotation file (int8)."""
-        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
-            print(f"Zarr File: {zarr_filename}")
 
+        def check_zarray(header_data, _zarr_filename):
+            del _zarr_filename
             zarrays = header_data["zarrays"]
             assert len(zarrays) == 3
-            for i, zarray in zarrays.items():
-                print(f"\t\tZarray: {i}")
+            for zarray in zarrays.values():
                 assert np.dtype(zarray["dtype"]) == np.int8
+
+        self.zarr_header_helper(seg_mask_annotation_zarr_headers, check_zarray)
 
     ### END ZARR Self-consistency tests ###
 
@@ -273,17 +317,17 @@ class TestSegmentationMaskHeader:
         seg_mask_annotation_mrc_headers: Dict[str, MrcInterpreter],
     ):
         """Check that the zarr volume size matches the mrc volume size, for each zarr annotation file and its binnings."""
-        for zarr_filename, header_data in seg_mask_annotation_zarr_headers.items():
-            print(f"\tZarr File: {zarr_filename}")
+
+        def check_volume_size(header_data, zarr_filename):
             mrc_file = zarr_filename.replace(".zarr", ".mrc")
-            print(f"\tMRC File: {mrc_file}")
 
             zarrays = header_data["zarrays"]
             for i, zarray in zarrays.items():
-                print(f"\t\tZarray: {i}")
                 header = seg_mask_annotation_mrc_headers[mrc_file].header
                 unbinned_zarray_shape = np.array(zarray["shape"]) * 2**i
                 assert (unbinned_zarray_shape == [header.nz, header.ny, header.nx]).all()
+
+        self.zarr_header_helper(seg_mask_annotation_zarr_headers, check_volume_size)
 
     ### END Cross-format consistency tests ###
 
@@ -295,12 +339,13 @@ class TestSegmentationMaskHeader:
     ):
         """Check that the annotation volume is contained within the canonical tomogram dimensions."""
 
-        def check_contained_in_tomo(header, _, canonical_tomogram_metadata):
+        def check_contained_in_tomo(header, _interpreter, _mrc_filename, canonical_tomogram_metadata):
+            del _interpreter, _mrc_filename
             assert header.nx == canonical_tomogram_metadata["size"]["x"]
             assert header.ny == canonical_tomogram_metadata["size"]["y"]
             assert header.nz == canonical_tomogram_metadata["size"]["z"]
 
-        self.header_helper(
+        self.mrc_header_helper(
             seg_mask_annotation_mrc_headers,
             check_contained_in_tomo,
             canonical_tomogram_metadata=canonical_tomogram_metadata,
