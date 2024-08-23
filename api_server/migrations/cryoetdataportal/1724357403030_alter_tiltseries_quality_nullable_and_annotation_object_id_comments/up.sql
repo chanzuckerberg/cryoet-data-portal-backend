60c0b863452283493b1059fdffe665bfbb0d6128
Daniel Ji <daji@ucsd.edu>
2024-08-22 18:25:24 -0700

    actual fix

--- api_server/migrations/cryoetdataportal/1724357403030_alter_tiltseries_quality_nullable_and_annotation_object_id_comments/up.sql
+++ api_server/migrations/cryoetdataportal/1724357403030_alter_tiltseries_quality_nullable_and_annotation_object_id_comments/up.sql
@@ -1,2 +1,2 @@
 comment on column "public"."annotations"."object_id" is E'Gene Ontology Cellular Component identifier or UniProtKB accession for the annotation object.';
-alter table "public"."tiltseries"."tiltseries_quality" alter column "annotation_object_id" drop not null;
+alter table "public"."tiltseries" alter column "tilt_series_quality" drop not null;
