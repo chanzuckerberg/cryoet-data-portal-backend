e7ca3c7642ae8f3260056eff35297609e82eb4bf
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2024-09-06 11:00:18 -0700

    Refactoring id helper

--- ingestion_tools/scripts/common/id_helper.py
+++ ingestion_tools/scripts/common/id_helper.py
@@ -7,51 +7,71 @@ from typing import Any
 from common.config import DepositionImportConfig
 
 
+# This class is a global var that caches metadata and identifiers for entities,
+# so we can generate non-conflicting sequential identifiers for entities as they're imported.
 class IdentifierHelper:
     next_identifier: dict[str, int] = defaultdict(partial(int, 100))
     cached_identifiers: dict[str, int] = {}
-    loaded_parent_containers: set[str] = {}
-
-    @classmethod
-    def _load_current_ids(cls, parent_key: str, config: DepositionImportConfig, *args, **kwargs):
-        if parent_key in cls.loaded_parent_containers:
-            return
-        metadata_glob = cls._get_metadata_glob(config, *args, **kwargs)
-        for file in config.fs.glob(metadata_glob):
-            identifier = int(os.path.basename(file).split("-")[0])
-            if identifier >= cls.next_identifier[parent_key]:
-                cls.next_identifier[parent_key] = identifier + 1
-            metadata = json.loads(config.fs.open(file, "r").read())
-            current_ids_key = cls._get_ids_key(parent_key, metadata, *args, **kwargs)
-            cls.cached_identifiers[current_ids_key] = identifier
-        cls.loaded_parent_containers.add(parent_key)
-
-    @classmethod
-    def _get_ids_key(cls, parent_key: str, metadata: dict[str, Any], parents: dict[str, Any], *args, **kwargs):
-        raise NotImplementedError("get_ids_key must be implemented in subclass")
+    loaded_containers: set[str] = {}
 
     @classmethod
     def get_identifier(
-        cls, config: DepositionImportConfig, metadata: dict[str, Any], parents: dict[str, Any], *args, **kwargs,
+        cls,
+        config: DepositionImportConfig,
+        metadata: dict[str, Any],
+        parents: dict[str, Any],
+        *args,
+        **kwargs,
     ):
-        parent_key = cls._get_parent_key(config, metadata, parents, *args, **kwargs)
-        cls._load_current_ids(parent_key, config, *args, **kwargs)
+        container_key = cls._get_container_key(config, metadata, parents, *args, **kwargs)
+        cls._load_ids_for_container(container_key, config, parents, *args, **kwargs)
 
-        current_ids_key = cls._get_ids_key(parent_key, metadata, parents, *args, **kwargs)
+        current_ids_key = cls._generate_hash_key(container_key, metadata, parents, *args, **kwargs)
         if cached_id := cls.cached_identifiers.get(current_ids_key):
             return cached_id
 
-        return_value = cls.next_identifier[parent_key]
+        return_value = cls.next_identifier[container_key]
         cls.cached_identifiers[current_ids_key] = return_value
-        cls.next_identifier[parent_key] += 1
+        cls.next_identifier[container_key] += 1
         return return_value
 
     @classmethod
-    def _get_metadata_glob(cls, config: DepositionImportConfig, *args, **kwargs) -> str:
-        raise NotImplementedError("get_metadata_glob must be implemented in subclass")
+    def _load_ids_for_container(
+        cls, container_key: str, config: DepositionImportConfig, parents: dict[str, Any], *args, **kwargs,
+    ):
+        if container_key in cls.loaded_containers:
+            return
+        metadata_glob = cls._get_metadata_glob(config, parents, *args, **kwargs)
+        for file in config.fs.glob(metadata_glob):
+            identifier = int(os.path.basename(file).split("-")[0])
+            if identifier >= cls.next_identifier[container_key]:
+                cls.next_identifier[container_key] = identifier + 1
+            metadata = json.loads(config.fs.open(file, "r").read())
+            current_ids_key = cls._generate_hash_key(container_key, metadata, *args, **kwargs)
+            cls.cached_identifiers[current_ids_key] = identifier
+        cls.loaded_containers.add(container_key)
 
     @classmethod
-    def _get_parent_key(
-        cls, config: DepositionImportConfig, metadata: dict[str, Any], parents: dict[str, Any], *args, **kwargs,
+    def _generate_hash_key(
+        cls, container_key: str, metadata: dict[str, Any], parents: dict[str, Any], *args, **kwargs,
+    ) -> str:
+        # Select specific values from metadata for hash generation
+        # This needs to be adjusted based on the structure of each metadata
+        raise NotImplementedError("_generate_hash_key must be implemented in subclass")
+
+    @classmethod
+    def _get_metadata_glob(cls, config: DepositionImportConfig, parents: dict[str, Any], *args, **kwargs) -> str:
+        # Glob for fetching relevant metadata files
+        raise NotImplementedError("_get_metadata_glob must be implemented in subclass")
+
+    @classmethod
+    def _get_container_key(
+        cls,
+        config: DepositionImportConfig,
+        metadata: dict[str, Any],
+        parents: dict[str, Any],
+        *args,
+        **kwargs,
     ):
-        raise NotImplementedError("get_ids_key must be implemented in subclass")
+        # This is value will be used to ensure we don't _load_current_ids for a folder multiple times
+        raise NotImplementedError("_get_container_key must be implemented in subclass")
