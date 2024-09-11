039a2f823b5bf292fccf0efe479742dd2e7606db
Jessica Gadling <jgadling@chanzuckerberg.com>
2024-09-11 15:25:43 -0400

    Start to prep db import for apiv2

---
+++ apiv2/db_import/importers/deposition.py
@@ -0,0 +1,137 @@
+from datetime import datetime, timezone
+from typing import Any, Iterable
+
+from database import models
+from platformics.database.models import Base
+from idb_import.importers.base_importer import AuthorsStaleDeletionDBImporter, BaseDBImporter, DBImportConfig
+
+
+def to_datetime(ts: int | None) -> datetime:
+    return datetime.fromtimestamp(ts, tz=timezone.utc) if ts else None
+
+
+class DepositionDBImporter(BaseDBImporter):
+    def __init__(self, dir_prefix: str, config: DBImportConfig):
+        self.config = config
+        self.dir_prefix = dir_prefix
+        self.parent = None
+        self.metadata = config.load_key_json(self.get_metadata_file_path())
+
+    def get_metadata_file_path(self) -> str:
+        return self.join_path(self.dir_prefix, "deposition_metadata.json")
+
+    def get_data_map(self) -> dict[str, Any]:
+        return {**self.get_direct_mapped_fields(), **self.get_computed_fields()}
+
+    @classmethod
+    def get_id_fields(cls) -> list[str]:
+        return ["id"]
+
+    @classmethod
+    def get_db_model_class(cls) -> type[Base]:
+        return models.Deposition
+
+    @classmethod
+    def get_direct_mapped_fields(cls) -> dict[str, Any]:
+        return {
+            "id": ["deposition_identifier"],
+            "title": ["deposition_title"],
+            "description": ["deposition_description"],
+            "deposition_date": ["dates", "deposition_date"],
+            "release_date": ["dates", "release_date"],
+            "last_modified_date": ["dates", "last_modified_date"],
+            "related_database_entries": ["cross_references", "related_database_entries"],
+            "deposition_publications": ["cross_references", "publications"],
+        }
+
+    def get_computed_fields(self) -> dict[str, Any]:
+        https_prefix = self.config.https_prefix
+        extra_data = {
+            "s3_prefix": self.join_path(self.config.s3_prefix, self.dir_prefix),
+            "https_prefix": self.join_path(https_prefix, self.dir_prefix),
+            "key_photo_url": None,
+            "key_photo_thumbnail_url": None,
+        }
+        deposition_type = self.metadata.get("deposition_types", [])
+        deposition_type.sort()
+        extra_data["deposition_types"] = ",".join(deposition_type)
+        key_photos = self.metadata.get("key_photos", {})
+        if snapshot_path := key_photos.get("snapshot"):
+            extra_data["key_photo_url"] = self.join_path(https_prefix, snapshot_path)
+        if thumbnail_path := key_photos.get("thumbnail"):
+            extra_data["key_photo_thumbnail_url"] = self.join_path(https_prefix, thumbnail_path)
+        return extra_data
+
+    @classmethod
+    def get_items(cls, config: DBImportConfig, deposition_id: int | str) -> Iterable["DepositionDBImporter"]:
+        return [
+            cls(deposition_id, config)
+            for deposition_id in config.find_subdirs_with_files(
+                f"depositions_metadata/{deposition_id}",
+                "deposition_metadata.json",
+            )
+        ]
+
+
+class DepositionAuthorDBImporter(AuthorsStaleDeletionDBImporter):
+    def __init__(self, deposition_id: int, parent: DepositionDBImporter, config: DBImportConfig):
+        self.deposition_id = deposition_id
+        self.parent = parent
+        self.config = config
+        self.metadata = parent.metadata.get("authors", [])
+
+    def get_data_map(self) -> dict[str, Any]:
+        return {
+            "deposition_id": self.deposition_id,
+            "orcid": ["ORCID"],
+            "name": ["name"],
+            "primary_author_status": ["primary_author_status"],
+            "corresponding_author_status": ["corresponding_author_status"],
+            "email": ["email"],
+            "affiliation_name": ["affiliation_name"],
+            "affiliation_address": ["affiliation_address"],
+            "affiliation_identifier": ["affiliation_identifier"],
+            "author_list_order": ["author_list_order"],
+        }
+
+    @classmethod
+    def get_id_fields(cls) -> list[str]:
+        return ["deposition_id", "name"]
+
+    @classmethod
+    def get_db_model_class(cls) -> type[Base]:
+        return models.DepositionAuthor
+
+    def get_filters(self) -> dict[str, Any]:
+        return {"deposition_id": self.deposition_id}
+
+    @classmethod
+    def get_item(
+        cls,
+        deposition_id: int,
+        parent: DepositionDBImporter,
+        config: DBImportConfig,
+    ) -> "DepositionAuthorDBImporter":
+        return cls(deposition_id, parent, config)
+
+
+# TODO: Make this a container that caches depositions so we aren't hitting the database multiple times for the same id
+def get_deposition(config: DBImportConfig, deposition_id: str | int) -> models.Deposition:
+    if not deposition_id:
+        raise ValueError(f"invalid deposition_id provided {deposition_id}")
+
+    deposition_id = int(deposition_id)
+    deposition = models.Deposition.get_or_none(deposition_id)
+    if deposition:
+        return deposition
+
+    depositions = []
+    for deposition_importer in DepositionDBImporter.get_items(config, deposition_id):
+        deposition_obj = deposition_importer.import_to_db()
+        depositions.append(deposition_obj)
+        deposition_authors = DepositionAuthorDBImporter.get_item(deposition_obj, deposition_importer, config)
+        deposition_authors.import_to_db()
+
+    if not depositions:
+        raise ValueError(f"Deposition {deposition_id} not found")
+    return depositions[0]
---
+++ apiv2/db_import/importers/tomogram.py
@@ -0,0 +1,171 @@
+import json
+from database import models
+from platformics.database.models import Base
+from typing import Any, Iterator
+
+from db_import.importers.deposition import get_deposition
+from common.normalize_fields import normalize_fiducial_alignment
+from db_import.importers.base_importer import (
+    AuthorsStaleDeletionDBImporter,
+    BaseDBImporter,
+    DBImportConfig,
+    StaleParentDeletionDBImporter,
+)
+from db_import.importers.voxel_spacing import TomogramVoxelSpacingDBImporter
+
+
+class TomogramDBImporter(BaseDBImporter):
+    def __init__(
+        self,
+        voxel_spacing_id: int,
+        dir_prefix: str,
+        parent: TomogramVoxelSpacingDBImporter,
+        config: DBImportConfig,
+    ):
+        self.voxel_spacing_id = voxel_spacing_id
+        self.dir_prefix = dir_prefix
+        self.parent = parent
+        self.config = config
+        self.metadata = config.load_key_json(self.get_metadata_file_path())
+
+    def get_metadata_file_path(self) -> str:
+        return self.join_path(self.dir_prefix, "tomogram_metadata.json")
+
+    def get_data_map(self) -> dict[str, Any]:
+        return {**self.get_direct_mapped_fields(), **self.get_computed_fields()}
+
+    @classmethod
+    def get_id_fields(cls) -> list[str]:
+        return ["name", "tomogram_voxel_spacing_id"]
+
+    @classmethod
+    def get_db_model_class(cls) -> type[Base]:
+        return models.Tomogram
+
+    @classmethod
+    def get_direct_mapped_fields(cls) -> dict[str, Any]:
+        return {
+            "name": ["run_name"],
+            "size_x": ["size", "x"],
+            "size_y": ["size", "y"],
+            "size_z": ["size", "z"],
+            "voxel_spacing": ["voxel_spacing"],
+            "processing": ["processing"],
+            "processing_software": ["processing_software"],
+            "tomogram_version": ["tomogram_version"],
+            "ctf_corrected": ["ctf_corrected"],
+            "offset_x": ["offset", "x"],
+            "offset_y": ["offset", "y"],
+            "offset_z": ["offset", "z"],
+            "affine_transformation_matrix": ["affine_transformation_matrix"],
+            "deposition_id": ["deposition_id"],
+        }
+
+    def normalize_to_unknown_str(self, value: str) -> str:
+        return value if value else "Unknown"
+
+    def get_tomogram_type(self) -> str:
+        if "CanonicalTomogram" in self.dir_prefix:
+            return "CANONICAL"
+        return "UNKOWN"
+
+    def generate_neuroglancer_data(self) -> str:
+        path = self.join_path(self.dir_prefix, "neuroglancer_config.json")
+        config = self.config.load_key_json(path, is_file_required=False)
+        # TODO: Log warning
+        return json.dumps(config, separators=(",", ":")) if config else "{}"
+
+    def get_computed_fields(self) -> dict[str, Any]:
+        https_prefix = self.config.https_prefix
+        s3_prefix = self.config.s3_prefix
+        extra_data = {
+            "tomogram_voxel_spacing_id": self.voxel_spacing_id,
+            "fiducial_alignment_status": normalize_fiducial_alignment(self.metadata.get("fiducial_alignment_status")),
+            "reconstruction_method": self.normalize_to_unknown_str(self.metadata.get("reconstruction_method")),
+            "reconstruction_software": self.normalize_to_unknown_str(self.metadata.get("reconstruction_software")),
+            "is_canonical": True,  # TODO: mark this for deprecation
+            "s3_omezarr_dir": self.join_path(s3_prefix, self.dir_prefix, self.metadata["omezarr_dir"]),
+            "https_omezarr_dir": self.join_path(https_prefix, self.dir_prefix, self.metadata["omezarr_dir"]),
+            "s3_mrc_scale0": self.join_path(s3_prefix, self.dir_prefix, self.metadata["mrc_files"][0]),
+            "https_mrc_scale0": self.join_path(https_prefix, self.dir_prefix, self.metadata["mrc_files"][0]),
+            "key_photo_url": None,
+            "key_photo_thumbnail_url": None,
+            "neuroglancer_config": self.generate_neuroglancer_data(),
+            "type": self.get_tomogram_type(),
+        }
+        if key_photos := self.metadata.get("key_photo"):
+            extra_data["key_photo_url"] = self.join_path(https_prefix, key_photos.get("snapshot"))
+            extra_data["key_photo_thumbnail_url"] = self.join_path(https_prefix, key_photos.get("thumbnail"))
+
+        for i in range(0, 3):
+            scale = self.metadata["scales"][i]
+            extra_data[f"scale{i}_dimensions"] = ",".join([str(scale[p]) for p in "xyz"])
+
+        deposition = get_deposition(self.config, self.metadata.get("deposition_id"))
+        extra_data["deposition_id"] = deposition.id
+        return extra_data
+
+    @classmethod
+    def get_item(
+        cls,
+        voxel_spacing_id: int,
+        voxel_spacing: TomogramVoxelSpacingDBImporter,
+        config: DBImportConfig,
+    ) -> Iterator["TomogramDBImporter"]:
+        tomogram_dir_path = cls.join_path(voxel_spacing.dir_prefix, "CanonicalTomogram")
+        return [
+            cls(voxel_spacing_id, tomogram_prefix, voxel_spacing, config)
+            for tomogram_prefix in config.find_subdirs_with_files(tomogram_dir_path, "tomogram_metadata.json")
+        ]
+
+
+class TomogramAuthorDBImporter(AuthorsStaleDeletionDBImporter):
+    def __init__(self, tomogram_id: int, parent: TomogramDBImporter, config: DBImportConfig):
+        self.tomogram_id = tomogram_id
+        self.parent = parent
+        self.config = config
+        self.metadata = parent.metadata.get("authors", [])
+
+    def get_data_map(self) -> dict[str, Any]:
+        return {
+            "tomogram_id": self.tomogram_id,
+            "orcid": ["ORCID"],
+            "name": ["name"],
+            "primary_author_status": ["primary_author_status"],
+            "corresponding_author_status": ["corresponding_author_status"],
+            "email": ["email"],
+            "affiliation_name": ["affiliation_name"],
+            "affiliation_address": ["affiliation_address"],
+            "affiliation_identifier": ["affiliation_identifier"],
+            "author_list_order": ["author_list_order"],
+        }
+
+    @classmethod
+    def get_id_fields(cls) -> list[str]:
+        return ["tomogram_id", "name"]
+
+    @classmethod
+    def get_db_model_class(cls) -> type[Base]:
+        return models.TomogramAuthor
+
+    def get_filters(self) -> dict[str, Any]:
+        return {"tomogram_id": self.tomogram_id}
+
+    @classmethod
+    def get_item(
+        cls,
+        dataset_id: int,
+        parent: TomogramDBImporter,
+        config: DBImportConfig,
+    ) -> "TomogramAuthorDBImporter":
+        return cls(dataset_id, parent, config)
+
+
+class StaleTomogramDeletionDBImporter(StaleParentDeletionDBImporter):
+    ref_klass = TomogramDBImporter
+
+    def get_filters(self) -> dict[str, Any]:
+        return {"tomogram_voxel_spacing_id": self.parent_id}
+
+    def children_tables_references(self) -> dict[str, None]:
+        return {"authors": None}
