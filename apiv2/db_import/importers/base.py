79f9ca50deee84d68e405acb757a4394a9b19d1d
Jessica Gadling <jgadling@chanzuckerberg.com>
2024-10-10 16:26:03 -0400

    More docs.

--- apiv2/db_import/importers/base.py
+++ apiv2/db_import/importers/base.py
@@ -20,8 +20,13 @@ logger = logging.getLogger("db_import")
 
 
 class ItemDBImporter:
+    """Abstract class that represents a single row that's been read as input data from S3 and needs to be imported into the DB"""
+
+    # Which sqlalchemy model class is being imported?
     model_class: Type[Base]
+    # A map between model properties and their locations in the input data.
     direct_mapped_fields: dict[str, list[str]] = {}
+    # Which fields uniquely identify a row?
     id_fields: list[str] = []
 
     def __init__(self, config: DBImportConfig, input_data: dict[str, Any]):
@@ -30,11 +35,12 @@ class ItemDBImporter:
         self.model_args = {}
 
     def _map_direct_fields(self):
-        # Load direct mapped fields
+        """Iterate over `self.direct_mapped_fields` and populate model args based on the data we find in the input dict."""
         for db_key, _ in self.direct_mapped_fields.items():
             self.model_args[db_key] = map_to_value(db_key, self.direct_mapped_fields, self.input_data)
 
     def load_computed_fields(self):
+        """If any data needs to be computed rather than copied over directly from the input dict, override this method."""
         pass
 
     def _get_identifiers(self) -> dict[str, Any]:
@@ -74,6 +80,11 @@ class ItemDBImporter:
 
 
 class IntegratedDBImporter(ABC):
+    """
+    Base class that manages importing many rows of a given type to the DB. It uses Finders to find
+    data that needs to be imported, and then optionally cleans up stale rows.
+    """
+
     finder: Type[ItemFinder]
     row_importer: Type[ItemDBImporter]
     # If true, will delete stale records after importing a batch of new/updated rows.
@@ -90,14 +101,39 @@ class IntegratedDBImporter(ABC):
         """Gets arguments to this class's cls.finder constructor."""
         pass
 
+    def import_items(self):
+        """
+        Find all items that need to be imported, import them, and clean up any stale items.
+        """
+        session = self.config.get_db_session()
+
+        finder = self.finder(self.config, **self.get_finder_args())
+        items = finder.find(self.row_importer, self.parents)
+        db_objects = [item.load(session) for item in items]
+        session.flush()
+        self.cleanup_unused_items(session, db_objects)
+        session.flush()
+        return db_objects
+
     @abstractmethod
     def get_filters(self) -> dict[str, Any]:
         """
-        This method finds all the existing records in the DB that belong to this Importer object.
-        It's used to determine which records are stale (as in, which ones weren't updated by this
-        importer's latest run) and need to be deleted.
+        This method supplies query filters that identify all the existing records in the DB that belong to
+        this Importer object. It's used to determine which records are stale (as in, which ones weren't
+        updated by this importer's latest run) and need to be deleted.
         """
         pass
+
+    def get_existing_objects(self, session) -> dict[int, Base]:
+        """
+        Creates a map of existing items by querying using the filter criteria. The map is keyed on the item id
+        """
+        records = session.scalars(
+            sa.select(self.row_importer.model_class).where(
+                sa.and_(*[getattr(self.row_importer.model_class, k) == v for k, v in self.get_filters().items()]),
+            ),
+        ).all()
+        return {record.id: record for record in records}
 
     def cleanup_unused_items(self, session, imported_objects):
         """
@@ -113,28 +149,3 @@ class IntegratedDBImporter(ABC):
         rows_to_delete = [row for id, row in existing_objs.items() if id not in keep_row_ids]
         for row in rows_to_delete:
             session.delete(row)
-
-    def import_items(self):
-        """
-        Find all items that need to be imported, import them, and clean up any stale items.
-        """
-        session = self.config.get_db_session()
-
-        finder = self.finder(self.config, **self.get_finder_args())
-        items = finder.find(self.row_importer, self.parents)
-        db_objects = [item.load(session) for item in items]
-        session.flush()
-        self.cleanup_unused_items(session, db_objects)
-        session.flush()
-        return db_objects
-
-    def get_existing_objects(self, session) -> dict[int, Base]:
-        """
-        Creates a map of existing items by querying using the filter criteria. The map is keyed on the item id
-        """
-        records = session.scalars(
-            sa.select(self.row_importer.model_class).where(
-                sa.and_(*[getattr(self.row_importer.model_class, k) == v for k, v in self.get_filters().items()]),
-            ),
-        ).all()
-        return {record.id: record for record in records}
