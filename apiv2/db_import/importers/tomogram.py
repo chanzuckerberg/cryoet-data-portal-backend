466b613976df69bae14ed410a8d33623a988a796
Jessica Gadling <jgadling@chanzuckerberg.com>
2024-10-10 14:36:35 -0400

    Beginning a refactor of the db importers.

--- apiv2/db_import/importers/tomogram.py
+++ apiv2/db_import/importers/tomogram.py
@@ -3,11 +3,11 @@ from typing import Any, Iterator
 
 import sqlalchemy as sa
 from database import models
+from db_import.common.config import DBImportConfig
 from db_import.common.normalize_fields import normalize_fiducial_alignment
 from db_import.importers.base_importer import (
     AuthorsStaleDeletionDBImporter,
     BaseDBImporter,
-    DBImportConfig,
     StaleParentDeletionDBImporter,
 )
 from db_import.importers.deposition import get_deposition
