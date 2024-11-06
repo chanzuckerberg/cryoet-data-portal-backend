3bd58025b36af80deda76e2a46bb13d3351bbb2a
uermel <uermel@czii.org>
2024-11-05 17:25:26 -0800

    prevent creating glob patterns as directories.

--- ingestion_tools/scripts/importers/annotation.py
+++ ingestion_tools/scripts/importers/annotation.py
@@ -156,7 +156,6 @@ class AnnotationImporter(BaseImporter):
     # Functions to support writing annotation metadata
     def get_output_path(self):
         output_dir = super().get_output_path().format(annotation_id=self.identifier)
-        print(f"Creating output directory {output_dir}")
         self.config.fs.makedirs(output_dir)
         return self.annotation_metadata.get_filename_prefix(output_dir)
 
