548b8d80ee384a857b1e37c15c50a3b53c2da2dc
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2024-11-07 12:04:48 -0800

    Adding tests

--- ingestion_tools/scripts/tests/s3_import/test_dataset_key_photos.py
+++ ingestion_tools/scripts/tests/s3_import/test_dataset_key_photos.py
@@ -1,14 +1,31 @@
-import json
 
+import numpy
+import pytest
 from importers.dataset import DatasetImporter
 from importers.dataset_key_photo import DatasetKeyPhotoImporter
 from importers.deposition import DepositionImporter
 from importers.utils import IMPORTERS
 from mypy_boto3_s3 import S3Client
+from PIL import Image
 
-from common.config import DepositionImportConfig
+from common.config import BaseImporter, DepositionImportConfig
 from common.fs import FileSystemApi
-from tests.s3_import.util import list_dir
+from tests.s3_import.util import get_children
+
+
+@pytest.fixture
+def add_tomo_image(s3_client: S3Client, test_output_bucket: str) -> None:
+    for image_format in ["snapshot", "thumbnail"]:
+        path = f"output/10001/TS_run1/Reconstructions/VoxelSpacing13.480/Images/100/key-photo-{image_format}.png"
+        imarray = numpy.random.rand(20, 20, 3) * 255
+        im = Image.fromarray(imarray.astype("uint8")).convert("RGBA")
+        s3_client.put_object(Bucket=test_output_bucket, Key=path, Body=im.tobytes())
+
+
+def get_parents(config: DepositionImportConfig) -> dict[str, BaseImporter]:
+    parents = {"deposition": list(DepositionImporter.finder(config))[0]}
+    parents["dataset"] = list(DatasetImporter.finder(config, **parents))[0]
+    return parents
 
 
 def test_key_photo_import_http(s3_fs: FileSystemApi, test_output_bucket: str, s3_client: S3Client) -> None:
@@ -17,25 +34,43 @@ def test_key_photo_import_http(s3_fs: FileSystemApi, test_output_bucket: str, s3
     output_path = f"{test_output_bucket}/{output_prefix}"
     input_bucket = "test-public-bucket"
     config = DepositionImportConfig(s3_fs, config_file, output_path, input_bucket, IMPORTERS)
+    key_photos = list(DatasetKeyPhotoImporter.finder(config, **get_parents(config)))
 
-    deposition = list(DepositionImporter.finder(config))[0]
-    dataset = list(DatasetImporter.finder(config, deposition=deposition))[0]
-    keyphotos = DatasetKeyPhotoImporter.finder(config, dataset=dataset)
-    for item in keyphotos:
+    for item in key_photos:
         item.import_item()
-    dataset.import_metadata()
 
-    with s3_fs.open(f"{output_path}/10001/dataset_metadata.json", "r") as fh:
-        output = fh.read()
-    metadata = json.loads(output)
-    assert metadata["dataset_title"] == "Dataset 1"
-    assert metadata["deposition_id"] == "10301"
+    # Check that the metadata is correct
+    metadata = key_photos[0].get_metadata()
+    # Check actual s3 files
+    s3_files = get_children(s3_client, test_output_bucket, f"{output_prefix}/10001/Images")
+    assert len(metadata) == 2
+    image_keys = {"snapshot", "thumbnail"}
+    for key in image_keys:
+        assert key in metadata
+        assert metadata[key] == f"10001/Images/{key}.png"
+        assert f"{key}.png" in s3_files
 
-    s3_files = list_dir(s3_client, test_output_bucket, f"{output_prefix}/10001/Images", assert_non_zero_size=True)
 
-    # Make sure the files are in our metadata and match our s3 file paths
-    for key, path in metadata["key_photos"].items():
-        assert key in ["snapshot", "thumbnail"]
-        assert path.startswith("10001/Images/")
-        assert f"{output_prefix}/{path}" in s3_files
-    assert len(metadata["key_photos"]) == 2
+def test_key_photo_import_from_tomogram(
+    s3_fs: FileSystemApi, test_output_bucket: str, s3_client: S3Client, add_tomo_image: None,
+) -> None:
+    config_file = "tests/fixtures/dataset2.yaml"
+    output_prefix = "output"
+    output_path = f"{test_output_bucket}/{output_prefix}"
+    input_bucket = "test-public-bucket"
+    config = DepositionImportConfig(s3_fs, config_file, output_path, input_bucket, IMPORTERS)
+    key_photos = list(DatasetKeyPhotoImporter.finder(config, **get_parents(config)))
+
+    for item in key_photos:
+        item.import_item()
+
+    # Check that the metadata is correct
+    metadata = key_photos[0].get_metadata()
+    # Check actual s3 files
+    s3_files = get_children(s3_client, test_output_bucket, f"{output_prefix}/10001/Images")
+    assert len(metadata) == 2
+    image_keys = {"snapshot", "thumbnail"}
+    for key in image_keys:
+        assert key in metadata
+        assert metadata[key] == f"10001/Images/{key}.png"
+        assert f"{key}.png" in s3_files
