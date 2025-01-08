bd7b0c2c3ed6ef7b5f61e31edd6e8191a96a712c
Jessica Gadling <jgadling@chanzuckerberg.com>
2025-01-08 10:28:20 -0500

    Add tests and support for deep_eq

--- apiv2/tests/test_gql_meta_support.py
+++ apiv2/tests/test_gql_meta_support.py
@@ -5,21 +5,23 @@ Test basic queries and mutations
 import datetime
 
 import pytest
+from conftest import deep_eq
 
 from platformics.database.connect import SyncDB
 from platformics.test_infra.factories.base import SessionStorage
+from test_infra.factories.dataset import DatasetFactory
 from test_infra.factories.deposition import DepositionFactory
 
 date_now = datetime.datetime.now()
 
 
 @pytest.mark.asyncio
-async def test_meta_fields_aggregate(
+async def test_top_meta_fields_aggregate(
     sync_db: SyncDB,
     gql_client,
 ) -> None:
     """
-    Test that we can filter datasets by the number of runs they have
+    Test that we can include gql meta fields at each level of an aggregate query
     """
 
     # Create mock data
@@ -55,3 +57,130 @@ async def test_meta_fields_aggregate(
         assert item["count"] == 1
         assert item["groupBy"]["__typename"] == "DepositionGroupByOptions"
         assert item["groupBy"]["id"] in dep_ids
+
+
+@pytest.mark.asyncio
+async def test_nested_meta_fields_aggregate(
+    sync_db: SyncDB,
+    gql_client,
+) -> None:
+    """
+    Test that we can include gql meta fields at each level of a nested aggregate query
+    """
+
+    # Create mock data
+    with sync_db.session() as session:
+        SessionStorage.set_session(session)
+        d1 = DepositionFactory.create(id=12345)
+        DatasetFactory.create(deposition=d1, id=222)
+
+    # Fetch all datasets that have at least one run
+    query = """
+        query MyQuery {
+          depositions {
+            __typename
+            id
+            datasetsAggregate {
+              __typename
+              aggregate {
+                __typename
+                count(columns: id, distinct: true)
+                sum {
+                  __typename
+                  id
+                }
+                groupBy {
+                  __typename
+                  id
+                }
+              }
+            }
+          }
+        }
+    """
+    output = await gql_client.query(query)
+    assert len(output["data"]["depositions"]) == 1
+    aggregate = output["data"]["depositions"][0]
+    assert aggregate["__typename"] == "Deposition"
+    assert aggregate["id"] == 12345
+
+    agg_info = aggregate["datasetsAggregate"]
+    assert agg_info["__typename"] == "DatasetAggregate"
+    assert len(agg_info["aggregate"]) == 1
+
+    agg_data = agg_info["aggregate"][0]
+    assert agg_data["__typename"] == "DatasetAggregateFunctions"
+    assert agg_data["count"] == 1
+    assert agg_data["sum"]["__typename"] == "DatasetNumericalColumns"
+    assert agg_data["sum"]["id"] == 222
+    assert agg_data["groupBy"]["__typename"] == "DatasetGroupByOptions"
+    assert agg_data["groupBy"]["id"] == 222
+
+
+@pytest.mark.asyncio
+async def test_nested_meta_fields_resolver(
+    sync_db: SyncDB,
+    gql_client,
+) -> None:
+    """
+    Test that we can include gql meta fields at each level of a nested NON-aggregate query
+    """
+
+    # Create mock data
+    with sync_db.session() as session:
+        SessionStorage.set_session(session)
+        d1 = DepositionFactory.create(id=12345)
+        DatasetFactory.create(deposition=d1, id=222)
+
+    # Fetch all datasets that have at least one run
+    query = """
+        query MyQuery {
+          depositions {
+            __typename
+            id
+            datasets {
+              __typename
+              edges {
+                __typename
+                node {
+                  __typename
+                  id
+                  deposition {
+                    __typename
+                    id
+                  }
+                }
+              }
+            }
+          }
+        }
+    """
+    output = await gql_client.query(query)
+
+    expected = {
+        "data": {
+            "depositions": [
+                {
+                    "__typename": "Deposition",
+                    "id": 12345,
+                    "datasets": {
+                        "__typename": "DatasetConnection",
+                        "edges": [
+                            {
+                                "__typename": "DatasetEdge",
+                                "node": {
+                                    "__typename": "Dataset",
+                                    "id": 222,
+                                    "deposition": {
+                                        "__typename": "Deposition",
+                                        "id": 12345,
+                                    },
+                                },
+                            },
+                        ],
+                    },
+                },
+            ],
+        },
+    }
+    assert deep_eq(output, expected)
