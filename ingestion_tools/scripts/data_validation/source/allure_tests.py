bd69e5cd0ba322852f7c6a20e0cd0dfcfb9a2176
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2025-01-30 10:25:28 -0800

    Adding allure test runner

---
+++ ingestion_tools/scripts/data_validation/source/allure_tests.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+import click
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(CURRENT_DIR, "..", ".."))
+
+from data_validation.shared.allure_runner import common_options, execute_allure_tests, update_with_default  # noqa: E402
+
+DEFAULT_OVERRIDES = {
+    "output_dir": "source_validation",
+}
+
+
+@click.command()
+@click.option(
+    "--ingestion-config",
+    type=str,
+    help="Path to the ingestion config file relative to dataset_config folder.",
+)
+@common_options
+def main(
+    ingestion_config: str,
+    **kwargs,
+):
+    config_name = os.path.basename(ingestion_config).split(".")[0]
+    updated_kwargs = update_with_default(kwargs, DEFAULT_OVERRIDES)
+    additional_parameters = [
+        f"--ingestion-config {ingestion_config}",
+        f"--input-bucket {updated_kwargs['input_bucket']}",
+    ]
+
+    execute_allure_tests(
+        identifier_name=config_name,
+        additional_parameters=additional_parameters,
+        default_overrides=DEFAULT_OVERRIDES,
+        **updated_kwargs,
+    )
+
+
+if __name__ == "__main__":
+    main()
--- ingestion_tools/scripts/data_validation/standardized/allure_tests.py
+++ ingestion_tools/scripts/data_validation/standardized/allure_tests.py
@@ -1,80 +1,32 @@
-import datetime
-import json
 import os
 import sys
-import tarfile
 
 import click
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(CURRENT_DIR, "..", ".."))
+
 from common.fs import FileSystemApi, S3Filesystem  # noqa: E402
+from data_validation.shared.allure_runner import common_options, execute_allure_tests, update_with_default  # noqa: E402
 
 STAGING_BUCKET = "cryoet-data-portal-staging"
-OUTPUT_BUCKET = "cryoetportal-output-test"
-
-
-def get_history(tar_report: str, destination: str, fs: FileSystemApi):
-    with fs.open(tar_report, "rb") as f, tarfile.open(fileobj=f, mode="r:gz") as t:
-        for file in t.getmembers():
-            if "/history/" in file.name and file.isfile():
-                file.name = os.path.basename(file.name)
-                t.extract(file, path=f"{destination}/history")
+DEFAULT_OVERRIDES = {
+    "output_dir": "data_validation",
+    "input_bucket": STAGING_BUCKET,
+}
 
 
 @click.command()
-@click.option(
-    "--local-dir",
-    default="./results",
-    type=str,
-    help="Local directory to store the results. Note that if this folder is in the data_validation folder (as \
-      is the default value), it should be added to the pytest call via `--ignore=[FOLDER_NAME]` to prevent pytest from \
-      wasting time trying to discover tests (we default ignore ./results in `pytest.ini`.)",
-)
-@click.option("--input-bucket", default=STAGING_BUCKET, type=str, help="S3 bucket to search for datasets.")
-@click.option("--output-bucket", default=OUTPUT_BUCKET, type=str, help="S3 bucket to store the report.")
-@click.option("--output-dir", default="data_validation", type=str, help="Output directory in the S3 bucket.")
 @click.option("--datasets", default="*", type=str, help="Comma separated list of dataset IDs.")
-@click.option(
-    "--multiprocessing/--no-multiprocessing",
-    is_flag=True,
-    default=True,
-    help="Run tests simultaneously with multiple workers (pytest-xdist).",
-)
-@click.option(
-    "--history/--no-history",
-    is_flag=True,
-    default=True,
-    help="Save the history to S3 and retrieve the history of the report. If testing multiple datasets, \
-    saving history will result in longer execution time (each dataset has to be an individual `pytest` call).",
-)
-@click.option(
-    "--extra-args",
-    default=None,
-    type=str,
-    help="Extra arguments to pass to pytest (in one string).",
-)
-@click.option(
-    "--update-s3/--no-update-s3",
-    is_flag=True,
-    default=True,
-    help="Write to S3 the results from this validation session.",
-)
+@common_options
 def main(
-    local_dir: str,
-    output_dir: str,
-    input_bucket: str,
-    output_bucket: str,
     datasets: str | None,
-    multiprocessing: bool,
-    history: bool,
-    extra_args: str | None,
-    update_s3: bool,
+    **kwargs,
 ):
     fs: S3Filesystem = FileSystemApi.get_fs_api(mode="s3", force_overwrite=False)
 
     if datasets is None:
-        datasets = fs.glob(f"s3://{input_bucket}/*")
+        datasets = fs.glob(f"s3://{kwargs['input_bucket']}/*")
         datasets = [os.path.basename(dataset) for dataset in datasets if os.path.basename(dataset).isdigit()]
     else:
         datasets = datasets.split(",")
@@ -82,54 +34,22 @@ def main(
     datasets = sorted(datasets, key=lambda x: int(x))
 
     # If we are not saving history, we can run all datasets in one go (individual dataset runs don't need to be tracked)
-    if not history:
+    if not kwargs["history"]:
         datasets = [",".join(datasets)]
 
-    extra_args = extra_args if extra_args else ""
-
+    updated_kwargs = update_with_default(kwargs, DEFAULT_OVERRIDES)
     for dataset in datasets:
-        now = datetime.datetime.now().isoformat(sep="_", timespec="seconds").replace(":", "-")
+        additional_parameters = [
+            f"--datasets {dataset}",
+            f"--bucket {kwargs['input_bucket']}",
+        ]
 
-        version = datetime.datetime.now().strftime("%Y%m%d%H%M%S")  # Needs to parse-able as an int.
-
-        # Accumulate test results here
-        localdir_raw = f"{local_dir}/{dataset}_raw/{now}"
-        # Generate report here
-        localdir_rep = f"{local_dir}/{dataset}/{now}"
-
-        # Run tests and generate results
-        multiprocessing_args = "--dist loadscope -n auto" if multiprocessing else "--dist no"
-        os.system(
-            f"pytest {multiprocessing_args} --datasets {dataset} --alluredir {localdir_raw} {extra_args}",
+        execute_allure_tests(
+            identifier_name=dataset,
+            additional_parameters=additional_parameters,
+            default_overrides=DEFAULT_OVERRIDES,
+            **updated_kwargs,
         )
-        # Allure needs this file in order to generate reports with history
-        with open(f"{localdir_raw}/executor.json", "w") as fh:
-            fh.write(
-                json.dumps(
-                    {
-                        "reportName": f"Build {dataset}",
-                        "buildOrder": version,
-                        "name": f"Portal Validation for dataset {dataset}",
-                    },
-                ),
-            )
-
-        # Get the history from S3 (Must do this before generating the report)
-        remote_tar_report = f"{output_bucket}/{output_dir}/{dataset}.tar.gz"
-        if fs.exists(remote_tar_report):
-            get_history(remote_tar_report, localdir_raw, fs)
-
-        # Generate the report
-        os.system(f"allure generate --output {localdir_rep} {localdir_raw}")
-
-        if update_s3:
-            # Compress and upload the new report
-            tar_report = f"{localdir_rep}.tar.gz"
-            print(f"Compressing {localdir_rep} to {tar_report} and replacing {remote_tar_report}")
-            os.system(f"tar -czf {tar_report} {localdir_rep}")
-            os.system(f"aws s3 sync {localdir_rep} s3://{output_bucket}/{output_dir}/{dataset}/")
-            fs.s3fs.put(tar_report, remote_tar_report, recursive=True)
-            os.remove(tar_report)
 
 
 if __name__ == "__main__":
