784c42eb68d0d9c1118c554a864b6294459b16a1
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2025-02-03 15:45:03 -0800

    Adding filter options to allure test

--- ingestion_tools/scripts/data_validation/source/allure_tests.py
+++ ingestion_tools/scripts/data_validation/source/allure_tests.py
@@ -6,11 +6,39 @@ import click
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(CURRENT_DIR, "..", ".."))
 
+from importers.utils import IMPORTERS  # noqa: E402
+
 from data_validation.shared.allure_runner import common_options, execute_allure_tests, update_with_default  # noqa: E402
 
 DEFAULT_OVERRIDES = {
     "output_dir": "source_validation",
 }
+
+
+def filter_options(func):
+    options = []
+    for cls in IMPORTERS:
+        importer_key = cls.type_key.replace("_", "-")
+        options.append(
+            click.option(
+                f"--filter-{importer_key}-name",
+                type=str,
+                default=None,
+                help=f"Filter pattern for {importer_key}",
+            ),
+        )
+    for option in options:
+        func = option(func)
+    return func
+
+
+def generate_filters(kwargs) -> str:
+    filters = []
+    for cls in IMPORTERS:
+        importer_key = cls.type_key.replace("_", "-")
+        if value := kwargs.get(f"filter_{importer_key}_name"):
+            filters.append(f"--filter-{importer_key}-name {value}")
+    return " ".join(filters) if filters else ""
 
 
 @click.command()
@@ -20,6 +48,7 @@ DEFAULT_OVERRIDES = {
     help="Path to the ingestion config file.",
 )
 @common_options
+@filter_options
 def main(
     ingestion_config: str,
     **kwargs,
@@ -29,6 +58,7 @@ def main(
     additional_parameters = [
         f"--input-bucket {updated_kwargs['input_bucket']}",
         f"--ingestion-config {ingestion_config}",
+        generate_filters(kwargs),
     ]
 
     execute_allure_tests(
