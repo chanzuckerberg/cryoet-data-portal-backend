ff26979bea8488a44481a6475b15eed6ae75d44f
Manasa Venkatakrishnan <mvenkatakrishnan@chanzuckerberg.com>
2025-02-04 10:59:19 -0800

    Adding per section parameter to tiltseries metadata

---
+++ ingestion_tools/scripts/common/ctf_converter.py
@@ -0,0 +1,46 @@
+from common.config import DepositionImportConfig
+
+
+class CTFInfo:
+    """CTF information for a single section of a tilt series.
+    Attributes:
+        section (int): Section number (1-based).
+        defocus_1 (float): Defocus value 1 (A).
+        defocus_2 (float): Defocus value 2 (A).
+        azimuth (float): Azimuth of astigmatism (deg).
+        phase_shift (float): Additional phase shift (radians).
+        cross_correlation (float): Cross correlation value.
+        max_resolution (float): Maximum resolution (A).
+    """
+    section: int = None
+    defocus_1: float = None
+    defocus_2: float = None
+    azimuth: float = None
+    phase_shift: float = None
+    cross_correlation: float = None
+    max_resolution: float = None
+
+
+DEFAULT_CTF_INFO = CTFInfo()
+
+
+class BaseCTFConverter:
+    def __init__(self, config: DepositionImportConfig, path: str):
+        self.path = path
+        self.config = config
+
+    @classmethod
+    def get_ctf_info(cls) -> list[CTFInfo]:
+        return []
+
+
+class AreTomo3CTF(BaseCTFConverter):
+    @classmethod
+    def get_ctf_info(cls) -> list[CTFInfo]:
+        return []
+
+def ctf_converter_factory(metadata: dict, config: DepositionImportConfig, path: str) -> BaseCTFConverter:
+    ctf_format = metadata.get("format")
+    if ctf_format == "CTFFIND":
+        return AreTomo3CTF(config, path)
+    return BaseCTFConverter(config, path)
--- ingestion_tools/scripts/importers/tiltseries.py
+++ ingestion_tools/scripts/importers/tiltseries.py
@@ -1,12 +1,19 @@
 import os.path
 from typing import Any
 
+import pandas as pd
+from mdocfile.data_models import MdocSectionData
+
 from common.config import DepositionImportConfig
+from common.ctf_converter import DEFAULT_CTF_INFO, CTFInfo
 from common.finders import DefaultImporterFactory
 from common.id_helper import IdentifierHelper
 from common.metadata import TiltSeriesMetadata
 from importers.base_importer import VolumeImporter
+from importers.collection_metadata import CollectionMetadataImporter
+from importers.ctf import CtfImporter
 from importers.frame import FrameImporter
+from importers.rawtilt import RawTiltImporter
 
 
 class TiltSeriesIdentifierHelper(IdentifierHelper):
@@ -92,6 +99,7 @@ class TiltSeriesImporter(VolumeImporter):
         merge_data["frames_count"] = self.get_frames_count()
         base_metadata = self.get_base_metadata()
         merge_data["pixel_spacing"] = self.get_pixel_spacing()
+        merge_data["per_section_parameter"] = self.get_per_section_parameter()
         metadata = TiltSeriesMetadata(self.config.fs, self.get_deposition().name, base_metadata)
         metadata.write_metadata(dest_ts_metadata, merge_data)
 
@@ -111,3 +119,56 @@ class TiltSeriesImporter(VolumeImporter):
         filename = metadata.get("omezarr_dir")
         complete_path = os.path.join(os.path.dirname(path), filename)
         return filename, complete_path, {filename: complete_path}
+
+    def get_per_section_parameter(self) -> list[dict[str, str]]:
+        psp = []
+        rawtlt = self.get_raw_tlt()
+        mdoc_data = self.get_mdoc_data()
+        ctf_data = self.get_ctf_data()
+        for index, raw_angle in rawtlt["raw_tlt_angle"].items():
+            mdoc_entry = self.get_mdoc_entry(round(raw_angle), mdoc_data)
+            ctf_entry = self.get_ctf_entry(mdoc_entry.ZValue, ctf_data)
+            psp_section = {
+                "raw_angle": mdoc_entry.TiltAngle,
+                "z-index": index,
+                "frame_acquistion_order": mdoc_entry.ZValue,
+                "astigmatic_angle": ctf_entry.azimuth,
+                "cross_correlation": ctf_entry.cross_correlation,
+                "major_defocus": ctf_entry.defocus_1,
+                "minor_defocus": ctf_entry.defocus_2,
+                "phase_shift": ctf_entry.phase_shift,
+                "max_resolution": ctf_entry.max_resolution,
+            }
+            psp.append(psp_section)
+
+        return psp
+
+    def get_raw_tlt(self) -> pd.DataFrame:
+        for rawtlt in RawTiltImporter.finder(self.config, **{**self.parents, "tiltseries": self}):
+            path = rawtlt.get_destination_path()
+            local_path = self.config.fs.localreadable(path)
+            return pd.read_csv(local_path, names=["raw_tlt_angle"])
+        raise Exception(f"No rawtlt found for run: {self.get_run().name}")
+
+    def get_mdoc_data(self) -> list[MdocSectionData]:
+        collection_md = CollectionMetadataImporter.get_importer(self.config, **self.parents)
+        return collection_md.get_output_data().section_data
+
+    def get_ctf_data(self) -> list[CTFInfo]:
+        for ctf in CtfImporter.finder(self.config, **{**self.parents, "tiltseries": self}):
+            return ctf.get_data()
+        return []
+
+    @classmethod
+    def get_mdoc_entry(cls, tilt_angle: float, mdoc_data: list[MdocSectionData]) -> MdocSectionData:
+        for entry in mdoc_data:
+            if round(entry.TiltAngle) == tilt_angle:
+                return entry
+        raise Exception(f"No match for tiltangle {tilt_angle} in mdoc_data")
+
+    @classmethod
+    def get_ctf_entry(cls, section_id: int, ctf_data: list[CTFInfo]) -> CTFInfo:
+        for entry in ctf_data:
+            if section_id == entry.section - 1:
+                return entry
+        return DEFAULT_CTF_INFO
