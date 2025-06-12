4158d21e3fedd3939ab2e89b7c0a331770d9275d
Sean Martin <martins7@tcd.ie>
2025-06-12 11:35:01 +0200

    feat: add single mesh precompute

--- ingestion_tools/scripts/importers/visualization_precompute.py
+++ ingestion_tools/scripts/importers/visualization_precompute.py
@@ -9,6 +9,7 @@ from importers.annotation import (
     AbstractPointAnnotation,
     BaseAnnotationSource,
     InstanceSegmentationAnnotation,
+    TriangularMeshAnnotation,
     VolumeAnnotationSource,
 )
 from importers.base_importer import BaseImporter
@@ -142,12 +143,37 @@ class SegmentationMaskAnnotationPrecompute(BaseAnnotationPrecompute):
         # module) cannot be imported successfully on darwin/ARM machines.
         from cryoet_data_portal_neuroglancer.precompute import segmentation_mask
 
-        resolution_in_nm = voxel_spacing * 0.1 # original in angstrom
+        resolution_in_nm = voxel_spacing * 0.1  # original in angstrom
         segmentation_mask.encode_segmentation(
             zarr_file_path,
             Path(tmp_path),
             resolution=(resolution_in_nm,) * 3,
             delete_existing=True,
             include_mesh=True,
+        )
+        fs.push(tmp_path)
+
+
+class MeshAnnotatationPrecompute(BaseAnnotationPrecompute):
+    annotation: TriangularMeshAnnotation
+
+    def neuroglancer_precompute(self, output_prefix: str, voxel_spacing: float) -> None:
+        fs = self.config.fs
+        annotation_path = self.annotation.get_output_path()
+        precompute_path = self._get_neuroglancer_precompute_path(annotation_path, output_prefix)
+        tmp_path = fs.localwritable(precompute_path)
+        glb_file_path = fs.destformat(self.annotation.get_output_filename(annotation_path, "glb"))
+        # Importing this at runtime instead of compile time since zfpy (a dependency of this
+        # module) cannot be imported successfully on darwin/ARM machines.
+        from cryoet_data_portal_neuroglancer.io import load_glb_file
+        from cryoet_data_portal_neuroglancer.precompute import mesh
+
+        scene = load_glb_file(glb_file_path)
+        mesh.generate_multiresolution_mesh(
+            scene,
+            tmp_path,
+            min_mesh_chunk_dim=8,
+            max_lod=2,
+            string_label=Path(annotation_path).stem.with_suffix("").replace(" ", "_"),
         )
         fs.push(tmp_path)
