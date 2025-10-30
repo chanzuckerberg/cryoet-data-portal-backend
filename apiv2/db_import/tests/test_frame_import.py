1c2d50e943f648a9c4c70198a410aeded5bd36d3
Ronen Kalo <rkalo@contractor.chanzuckerberg.com>
2025-10-30 14:37:21 -0400

    Fix tests

--- apiv2/db_import/tests/test_frame_import.py
+++ apiv2/db_import/tests/test_frame_import.py
@@ -28,7 +28,7 @@ def expected_frames(http_prefix: str) -> list[dict[str, Any]]:
             "is_gain_corrected": True,
             "s3_frame_path": "s3://test-public-bucket/30001/RUN1/Frames/frame2",
             "https_frame_path": "https://foo.com/30001/RUN1/Frames/frame2",
-            "file_size": 11.0,
+            "file_size": 0.0,
         },
         {
             "acquisition_order": 2,
