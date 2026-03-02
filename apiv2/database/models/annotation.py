6ce7fb41a9d1885d7596f192c13afc9a53347b48
PALLAVIKHEDLE <pallavi8khedle@gmail.com>
2026-03-02 14:07:30 -0800

    Add annotation_ingest_id as nullable column to annotation table

--- apiv2/database/models/annotation.py
+++ apiv2/database/models/annotation.py
@@ -76,6 +76,7 @@ class Annotation(Base):
     https_metadata_path: Mapped[str] = mapped_column(String, nullable=False)
     annotation_publication: Mapped[str] = mapped_column(String, nullable=True)
     annotation_method: Mapped[str] = mapped_column(String, nullable=False)
+    annotation_ingest_id: Mapped[str] = mapped_column(String, nullable=True)
     ground_truth_status: Mapped[bool] = mapped_column(Boolean, nullable=True, server_default="False")
     object_id: Mapped[str] = mapped_column(String, nullable=False)
     object_name: Mapped[str] = mapped_column(String, nullable=False)
