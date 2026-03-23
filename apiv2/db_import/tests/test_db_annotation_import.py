6c24e8bffa827e919cad214d3e0191ce1cde9df5
Jinghui Geng <jgeng@CZIMACOS5722.local>
2026-03-23 13:29:41 -0700

    clean up

--- apiv2/db_import/tests/test_db_annotation_import.py
+++ apiv2/db_import/tests/test_db_annotation_import.py
@@ -54,7 +54,6 @@ def expected_annotations(http_prefix: str) -> list[dict[str, Any]]:
 def expected_annotation_files(http_prefix: str) -> list[dict[str, Any]]:
     path = f"{DATASET_ID}/RUN1/Reconstructions/VoxelSpacing12.300/Annotations/"
     return [
-        # AnnotationCaption (sorted first by shape_type alphabetically)
         {
             "tomogram_voxel_spacing_id": TOMOGRAM_VOXEL_ID1,
             "s3_path": f"s3://test-public-bucket/{path}100-foo-1.0_point_caption.json",
@@ -64,7 +63,6 @@ def expected_annotation_files(http_prefix: str) -> list[dict[str, Any]]:
             "is_visualization_default": False,
             "file_size": 0,
         },
-        # GlobalCaption
         {
             "tomogram_voxel_spacing_id": TOMOGRAM_VOXEL_ID1,
             "s3_path": f"s3://test-public-bucket/{path}100-foo-1.0_globalcaption.json",
@@ -74,7 +72,6 @@ def expected_annotation_files(http_prefix: str) -> list[dict[str, Any]]:
             "is_visualization_default": False,
             "file_size": 0,
         },
-        # InstanceSegmentationMask (mrc then zarr, sorted by format)
         {
             "tomogram_voxel_spacing_id": TOMOGRAM_VOXEL_ID1,
             "s3_path": f"s3://test-public-bucket/{path}100-foo-1.0_instancesegmask.mrc",
@@ -93,7 +90,6 @@ def expected_annotation_files(http_prefix: str) -> list[dict[str, Any]]:
             "is_visualization_default": False,
             "file_size": 0,
         },
-        # Point
         {
             "id": ANNOTATION_FILE_ID,
             "tomogram_voxel_spacing_id": TOMOGRAM_VOXEL_ID1,
@@ -104,7 +100,6 @@ def expected_annotation_files(http_prefix: str) -> list[dict[str, Any]]:
             "is_visualization_default": True,
             "file_size": 0,
         },
-        # SegmentationMask (mrc then zarr)
         {
             "tomogram_voxel_spacing_id": TOMOGRAM_VOXEL_ID1,
             "s3_path": f"s3://test-public-bucket/{path}100-foo-1.0_segmask.mrc",
