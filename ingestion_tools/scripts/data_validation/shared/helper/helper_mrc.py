961e6cf954e7a40acb8e8f35f36321d3b8e18e55
Daniel Ji <daji@ucsd.edu>
2026-03-25 20:02:58 -0700

    fix test

--- ingestion_tools/scripts/data_validation/shared/helper/helper_mrc.py
+++ ingestion_tools/scripts/data_validation/shared/helper/helper_mrc.py
@@ -48,7 +48,7 @@ class HelperTestMRCHeader:
     """
 
     # Class variables that need to be set by the subclass
-    map_id: bytes = b"MAP "
+    map_id: str = "MAP "
     spacegroup: int = None
     mrc_headers: Dict[str, MrcInterpreter] = None
     spacing: float = None
